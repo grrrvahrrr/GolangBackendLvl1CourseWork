6df83e9159985660ee491d15c396287ab19c86c1
Sergei <kabernik@gmail.com>
2022-02-07 14:46:22 +0300

    some more changes

--- database/filestorage.go
+++ database/filestorage.go
@@ -2,7 +2,7 @@ package database
 
 import "context"
 
-type FullDataDB struct {
+type FullDataFile struct {
 	FullURL  string
 	ShortURL string
 	Data     string //to be remade into slice of strings of a map[string]string
@@ -12,30 +12,25 @@ type FullDataDB struct {
 //For now methods are skeletons of what they are
 
 //Writing to a file/db what we got from Backend after validating URLs and Updating Data
-func (fd *FullDataDB) WriteURL(ctx context.Context, fullURL string, shortURL string) error {
+func (fd *FullDataFile) WriteURL(ctx context.Context, fullURL string, shortURL string) error {
 	return nil
 }
 
-func (fd *FullDataDB) WriteData(ctx context.Context, shortURL string, data string) error {
+func (fd *FullDataFile) WriteData(ctx context.Context, shortURL string, data string) error {
 	return nil
 }
 
 //Reads info from file/DB
-type Reader interface {
-	ReadURL(ShortURL string) (FullURL string, err error)
-	ReadData(ShortURL string) (Data string, err error)
-}
-
-func (fd *FullDataDB) ReadURL(ShortURL string) (FullURL string, err error) {
+func (fd *FullDataFile) ReadURL(ctx context.Context, ShortURL string) (FullURL string, err error) {
 	return FullURL, nil
 }
 
-func (fd *FullDataDB) ReadData(ShortURL string) (Data string, err error) {
+func (fd *FullDataFile) ReadData(ctx context.Context, ShortURL string) (Data string, err error) {
 	return Data, nil
 }
 
 //Delete record if neccessary
-func (fd *FullDataDB) Delete(FullURL string, ShortURL string) error {
+func (fd *FullDataFile) Delete(ctx context.Context, FullURL string, ShortURL string) error {
 	return nil
 }
 
