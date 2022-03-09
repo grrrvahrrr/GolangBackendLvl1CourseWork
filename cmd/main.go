02de5e3c2690a0133a3332001ccaa84961a0112e
Sergei <kabernik@gmail.com>
2022-03-09 13:15:21 +0300

    more static

--- cmd/main.go
+++ cmd/main.go
@@ -38,7 +38,7 @@ func main() {
 	// 	log.Error(err)
 	// }
 
-	cfg, err := config.LoadConfig("./bitme/static/bitme/config.env")
+	cfg, err := config.LoadConfig("app/cmd/static/config.env")
 	if err != nil {
 		log.Fatal("Error loading config: ", err)
 	}
